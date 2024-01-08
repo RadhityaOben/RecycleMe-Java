b148cacb8a0ce7573ada82840a783501db864f98
Iqbal Maulana Sidiq <iqbal.213040061@mail.unpas.ac.id>
2024-01-08 22:08:51 +0700

    lanjutan pdf reporting

---
+++ src/main/java/com/recycleme/actionListener/reports/CetakPDF.java
@@ -0,0 +1,50 @@
+package com.recycleme.actionListener.reports;
+
+import com.recycleme.db.MySqlConnection;
+import com.recycleme.frame.reports.ReportsFrame;
+import net.sf.jasperreports.engine.*;
+
+import java.awt.event.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CetakPDF implements ActionListener {
+    private ReportsFrame reportsFrame;
+    private String reportType;
+
+    public CetakPDF(ReportsFrame reportsFrame) {
+        this.reportsFrame = reportsFrame;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(reportsFrame.getComboBoxPilihan().equals("Masyarakat")) {
+            reportType = "masyarakatReport";
+        } else if(reportsFrame.getComboBoxPilihan().equals("Kurir")) {
+            reportType = "kurirReport";
+        } else if(reportsFrame.getComboBoxPilihan().equals("Dropbox")) {
+            reportType = "dropboxReport";
+        }
+
+        try{
+            String frontPath = "src/main/resources/reports/";
+            String path = frontPath + reportType + ".jrxml";
+            String exportPath = "src/main/export/";
+            JasperReport jasperReport = JasperCompileManager.compileReport(path);
+
+            Map<String, Object> parameters = new HashMap<>();
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, MySqlConnection.getInstance().getConnection());
+
+            File outDir = new File(frontPath);
+            outDir.mkdirs();
+
+            JasperExportManager.exportReportToPdfFile(jasperPrint, exportPath + reportType + ".pdf");
+
+            reportsFrame.showSuccessMessage("Laporan berhasil dicetak");
+        } catch(JRException ex) {
+            reportsFrame.showErrorMessage("Laporan gagal dicetak");
+            ex.printStackTrace();
+        }
+    }
+}
